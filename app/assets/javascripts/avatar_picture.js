e8b614a26da2841ff272f633983337e8216bee2c
SonNguyen1604 <sonnguyenngoc1604@gmail.com>
2017-07-03 13:14:39 +0700

    Finish avatar

---
+++ app/assets/javascripts/avatar_picture.js
@@ -0,0 +1,12 @@
+$(document).on('turbolinks:load', function(){
+  $('#user_avatar').change(function(){
+    var input = $(this);
+    var file = input[0].files[0];
+    var reader = new FileReader();
+    reader.onload = function(e){
+      var image_base64 = e.target.result;
+      $('.picture img').attr('src', image_base64);
+    };
+    reader.readAsDataURL(file);
+  });
+});
